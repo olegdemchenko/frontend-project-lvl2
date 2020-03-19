1f16036bcda18137f83dccaa003525b239e3a76c
olegdemchenko <olegdemchenko1993@gmail.com>
2020-03-19 12:18:22 +0200

    fixing findDifferences.js

--- src/findDifferences.js
+++ src/findDifferences.js
@@ -2,11 +2,14 @@ import isObject from './utils';
 
 const _ = require('lodash');
 
-const findDifferences = (before, after) => {
+const analyzeData = (before, after) => {
   const commonKeys = _.intersection(Object.keys(before), Object.keys(after));
   const uniqueKeysBefore = _.difference(Object.keys(before), Object.keys(after));
   const uniqueKeysAfter = _.difference(Object.keys(after), Object.keys(before));
-  const difference = [...commonKeys, ...uniqueKeysBefore, ...uniqueKeysAfter].reduce((acc, key) => {
+  return [...commonKeys, ...uniqueKeysBefore, ...uniqueKeysAfter];
+};
+const findDifferences = (before, after) => (
+  [...analyzeData(before, after)].reduce((acc, key) => {
     if (_.has(before, key) && _.has(after, key)) {
       if (isObject(before[key]) && isObject(after[key])) {
         return [...acc, [key, [...findDifferences(before[key], after[key])]]];
@@ -20,7 +23,6 @@ const findDifferences = (before, after) => {
       return [...acc, [`- ${key}`, before[key]]];
     }
     return [...acc, [`+ ${key}`, after[key]]];
-  }, []);
-  return difference;
-};
+  }, [])
+);
 export default findDifferences;
