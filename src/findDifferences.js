a15f9737f0d48d3e4311b7fcea595079dcfecdcf
olegdemchenko <olegdemchenko1993@gmail.com>
2020-03-19 20:02:57 +0200

    refactor findDifferences.js try 1

--- src/findDifferences.js
+++ src/findDifferences.js
@@ -2,13 +2,41 @@ import isObject from './utils';
 
 const _ = require('lodash');
 
-
-const findDifferences = (before, after) => {
+const analyzeData = (before, after) => {
   const commonKeys = _.intersection(Object.keys(before), Object.keys(after));
   const uniqueKeysBefore = _.difference(Object.keys(before), Object.keys(after));
   const uniqueKeysAfter = _.difference(Object.keys(after), Object.keys(before));
-  const difference = [...commonKeys, ...uniqueKeysBefore, ...uniqueKeysAfter].reduce((acc, key) => {
-    if (_.has(before, key) && _.has(after, key)) {
+  return [...commonKeys, ...uniqueKeysBefore, ...uniqueKeysAfter];
+};
+const findDifferences = (before, after) => {
+  const difference = [...analyzeData(before, after)].reduce((acc, key) => {
+    const bothObjects = (
+      _.has(before, key)
+      && _.has(after, key)
+      && isObject(before[key])
+      && isObject(after[key]));
+    const notEqual = _.has(before, key) && _.has(after, key) && before[key] !== after[key];
+    const notBefore = !_.has(before, key) && _.has(after, key);
+    const notAfter = _.has(before, key) && !_.has(after, key);
+    if (bothObjects) {
+      return [...acc, [key, [...findDifferences(before[key], after[key])]]];
+    }
+    if (notEqual) {
+      return [...acc, [`- ${key}`, before[key]], [`+ ${key}`, after[key]]];
+    }
+    if (notBefore) {
+      return [...acc, [`+ ${key}`, after[key]]];
+    }
+    if (notAfter) {
+      return [...acc, [`- ${key}`, before[key]]];
+    }
+    return [...acc, [key, before[key]]];
+  }, []);
+  return difference;
+};
+export default findDifferences;
+/*
+if (_.has(before, key) && _.has(after, key)) {
       if (isObject(before[key]) && isObject(after[key])) {
         return [...acc, [key, [...findDifferences(before[key], after[key])]]];
       }
@@ -20,8 +48,4 @@ const findDifferences = (before, after) => {
     if (_.has(before, key) && !_.has(after, key)) {
       return [...acc, [`- ${key}`, before[key]]];
     }
-    return [...acc, [`+ ${key}`, after[key]]];
-  }, []);
-  return difference;
-};
-export default findDifferences;
+    return [...acc, [`+ ${key}`, after[key]]];*/
---
+++ testfile.js
@@ -0,0 +1,3 @@
+import genDiff from './src/genDiff';
+
+console.log(genDiff(`${__dirname}/__fixtures__/before.json`, `${__dirname}/__fixtures__/after.json`));
